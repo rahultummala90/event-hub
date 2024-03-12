73f73ee5f0a2a40d1828972bb980bcf1cdabba5d
Rahul Tummala <rahul@Rahuls-MBP.attlocal.net>
2024-03-12 01:56:26 -0400

    [AB#1] - Add Notifications and rate limiting

--- app/Http/Controllers/Api/AttendeeController.php
+++ app/Http/Controllers/Api/AttendeeController.php
@@ -13,6 +13,8 @@ class AttendeeController extends Controller
     public function __construct()
     {
         $this->middleware("auth:sanctum")->except(["index", "show", "update"]);
+        $this->middleware("throttle:api")
+            ->only(["store", "destroy"]);
         $this->authorizeResource(AttendeeResource::class, Attendee::class);
     }
 
@@ -32,7 +34,7 @@ class AttendeeController extends Controller
     public function store(Request $request, Event $event)
     {
         $attendee = $event->attendees()->create([
-            'user_id' => 1
+            'user_id' => $request->user()->id,
         ]);
 
         return new AttendeeResource($attendee);
